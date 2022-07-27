6bbaf3ec26ec960557503442f1d4a854a31fa351
Akshat-C <85225437+Akshat-C@users.noreply.github.com>
2022-07-27 19:23:09 +1000

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 video = "";
 status1 = false;
+objects = [];
 
 function preload()
 {
@@ -18,6 +19,36 @@ function setup()
 function draw()
 {
     image(video, 0, 0, 580, 430);
+
+    if (status1 != "")
+    {
+        od.detect(video, gotResult);
+        document.getElementById("status").innerHTML = "Objects Detected";
+        document.getElementById("num_obj").innerHTML = objects.length;
+
+        for (i=0; i<objects.length; i++)
+        {
+            n = objects[i].label;
+            per = Math.floor(objects[i].confidence*100);
+            fill('red');
+            text(n + " " + per + " %", objects[i].x+15, objects[i].y+15);
+            noFill();
+            stroke("red");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
+}
+
+function gotResult(error, results)
+{
+    if (error)
+    {
+        console.error(error);
+    } else 
+    {
+        console.log(results);
+        objects = results;
+    }
 }
 
 function start()
